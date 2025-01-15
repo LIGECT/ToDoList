ef546afe72e5dea9c39ab01cd14c8dcb71e504bc
Ilya Kutsenko <viminiko@gmail.com>
2025-01-15 19:38:40 +0200

    My local changes are more important

---
+++ todo/src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './assets/scss/normalize.scss'
+import './assets/scss/style.scss'
+import { ToDoListPage } from './pages/ToDoListPage'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ToDoListPage />
+  </StrictMode>
+)
