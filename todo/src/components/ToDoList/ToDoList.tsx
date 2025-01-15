f5d08404451b57fc84f02c5a993376e51cc3854c
Ilya Kutsenko <viminiko@gmail.com>
2025-01-15 21:35:41 +0200

    Add ToDo model and update ToDoList components to use props for task rendering

--- todo/src/components/ToDoList/ToDoList.tsx
+++ todo/src/components/ToDoList/ToDoList.tsx
@@ -1,15 +1,22 @@
-import './ToDoList.scss'
 import { ToDoListItem } from "./ToDoListItem/ToDoListItem"
+import './ToDoList.scss'
+import { ToDo } from '../../models/todo-item'
 
 export const ToDoList = () => {
+    const todo1: ToDo = {
+        id: 0,
+        text: 'Первая задача',
+        isDone: false
+    }
+
     return (
         <div className="todo-container">
             <ul className="todo-list failed">
-               <ToDoListItem />
+               <ToDoListItem toDoItem={todo1} />
             </ul>
             <ul className="todo-list completed">
 
-                <ToDoListItem />
+                {/* <ToDoListItem /> */}
                 {/* <li className="todo-list-item__wrapper">
                     <span>Вторая задача</span>
                     <div className="todo-list-item__buttons">
